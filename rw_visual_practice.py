b44945bdf0ccd15bd4a41acee8e885d33903c753
陈浩然 <chenhaoran@chenhaorandeMacBook-Pro.local>
2020-10-12 11:32:00 +0800

    增加plot形式、重构fill_walk()

--- rw_visual_practice.py
+++ rw_visual_practice.py
@@ -5,17 +5,17 @@ from random_walk import RandomWalk
 #只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     #创建一个RandomWalk实例，并将其包含的点都绘制出来
-    rw = RandomWalk(500000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     #设置绘图窗口的尺寸
     plt.figure(figsize=(10,6))
     #设置着色点列表
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values,rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
-    #突出起点和终点
-    plt.scatter(0,0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.plot(rw.x_values,rw.y_values, linewidth=4)
+    # #突出起点和终点
+    # plt.scatter(0,0, c='green', edgecolors='none', s=100)
+    # plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     
 
     #隐藏坐标轴
